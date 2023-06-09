806587b55439ea074e585bb9d1f508749059bbe4
Elisa Exposito <eliexme@gmail.com>
2023-06-09 11:42:13 +0100

    back working fine

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
+const data = require ('../data/phones.json')
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+});
+
+router.get("/phones", (req, res, next) => {
+  res.json(data);
+});
+
+router.get("/phones/:id", (req, res, next) => {
+  const id = parseInt(req.params.id)
+  const phoneDets = data.filter((eachPhone)=>{
+    return eachPhone.id === id
+  })
+  res.json(phoneDets);
 });
 
 module.exports = router;
